e07a4daa4de3ffd3402facdcc6f6cac20634bf93
tokizuoh <tokizuoh@gmail.com>
2022-04-20 17:26:45 +0900

    ログイン機能自体をserverコンテナからfrontコンテナに移動

--- front/main.go
+++ front/main.go
@@ -6,18 +6,63 @@ import (
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		t, err := template.ParseFiles("login.html")
-		if err != nil {
-			fmt.Fprintf(w, err.Error(), nil)
-			return
-		}
+func login(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("login.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error(), nil)
+		return
+	}
 
-		if err := t.Execute(w, nil); err != nil {
-			fmt.Fprintf(w, err.Error(), nil)
-			return
-		}
-	})
+	if err := t.Execute(w, nil); err != nil {
+		fmt.Fprintf(w, err.Error(), nil)
+		return
+	}
+}
+
+func validLogin(w http.ResponseWriter, r *http.Request) {
+	// TODO: HEADリクエストは受け付ける必要ある？
+	if r.Method != http.MethodPost {
+		statusCode := http.StatusMethodNotAllowed
+		http.Error(w, http.StatusText(statusCode), statusCode)
+		return
+	}
+
+	if err := r.ParseForm(); err != nil {
+		statusCode := http.StatusInternalServerError
+		http.Error(w, err.Error(), statusCode)
+		return
+	}
+
+	id := r.Form.Get("id")
+	password := r.Form.Get("password")
+
+	if !(id == "test-id" && password == "test-password") {
+		statusCode := http.StatusUnauthorized
+		http.Error(w, http.StatusText(statusCode), statusCode)
+		return
+	}
+
+	// TODO: セッションID発行してCookieに入れて送る
+	http.Redirect(w, r, "/home", http.StatusMovedPermanently)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	// TODO: 直接 /home にリクエストできてしまうので解決する
+	t, err := template.ParseFiles("home.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error(), nil)
+		return
+	}
+
+	if err := t.Execute(w, nil); err != nil {
+		fmt.Fprintf(w, err.Error(), nil)
+		return
+	}
+}
+
+func main() {
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/validLogin", validLogin)
+	http.HandleFunc("/home", home)
 	http.ListenAndServe(":8081", nil)
 }

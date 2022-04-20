3e3baef9b07940fb07a1c5bc7e3453c690cf8184
tokizuoh <tokizuoh@gmail.com>
2022-04-20 13:06:32 +0900

    GETリクエストを受け付ける処理を追加

--- server/main.go
+++ server/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	log.Println("Hello, World")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello, World")
+	})
+	http.ListenAndServe(":8080", nil)
 }

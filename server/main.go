a608318c9c735c18a501933d1accf6d8dc4bd094
tokizuoh <tokizuoh@gmail.com>
2022-04-20 14:27:40 +0900

    GET, HEADリクエスト以外を受け付けないようにした

--- server/main.go
+++ server/main.go
@@ -7,6 +7,10 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet && r.Method != http.MethodHead {
+			statusCode := http.StatusMethodNotAllowed
+			http.Error(w, http.StatusText(statusCode), statusCode)
+		}
 		fmt.Fprintln(w, "Hello, World")
 	})
 	http.ListenAndServe(":8080", nil)
